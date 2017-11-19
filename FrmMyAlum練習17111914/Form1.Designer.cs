fe41961b744d101d40770e96b599015282fd27b7
joelee <as955133@gmail.com>
2017-11-19 18:03:25 +0800

    錯誤產生目前暫無法解決 System.InvalidCastException: '無法將類型 'System.Data.DataRowView' 的物件轉換為類型 'System.String'。'

--- FrmMyAlum練習17111914/Form1.Designer.cs
+++ FrmMyAlum練習17111914/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnInsert = new System.Windows.Forms.Button();
+            this.BtnOpenFile = new System.Windows.Forms.Button();
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.lstPhotoType = new System.Windows.Forms.ListBox();
+            this.lstPhoto = new System.Windows.Forms.ListView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(996, 644);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.BtnClear);
+            this.tabPage1.Controls.Add(this.BtnDelete);
+            this.tabPage1.Controls.Add(this.BtnInsert);
+            this.tabPage1.Controls.Add(this.BtnOpenFile);
+            this.tabPage1.Controls.Add(this.BtnLoad);
+            this.tabPage1.Controls.Add(this.lstPhotoType);
+            this.tabPage1.Controls.Add(this.lstPhoto);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(988, 618);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(825, 494);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 23);
+            this.BtnClear.TabIndex = 7;
+            this.BtnClear.Text = "取消";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(731, 536);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 6;
+            this.BtnDelete.Text = "刪除";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            // 
+            // BtnInsert
+            // 
+            this.BtnInsert.Location = new System.Drawing.Point(731, 494);
+            this.BtnInsert.Name = "BtnInsert";
+            this.BtnInsert.Size = new System.Drawing.Size(75, 23);
+            this.BtnInsert.TabIndex = 5;
+            this.BtnInsert.Text = "新增";
+            this.BtnInsert.UseVisualStyleBackColor = true;
+            // 
+            // BtnOpenFile
+            // 
+            this.BtnOpenFile.Location = new System.Drawing.Point(731, 451);
+            this.BtnOpenFile.Name = "BtnOpenFile";
+            this.BtnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.BtnOpenFile.TabIndex = 4;
+            this.BtnOpenFile.Text = "檔案";
+            this.BtnOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Location = new System.Drawing.Point(731, 411);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(75, 23);
+            this.BtnLoad.TabIndex = 3;
+            this.BtnLoad.Text = "讀取";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // lstPhotoType
+            // 
+            this.lstPhotoType.FormattingEnabled = true;
+            this.lstPhotoType.ItemHeight = 12;
+            this.lstPhotoType.Location = new System.Drawing.Point(3, 19);
+            this.lstPhotoType.Name = "lstPhotoType";
+            this.lstPhotoType.Size = new System.Drawing.Size(183, 556);
+            this.lstPhotoType.TabIndex = 2;
+            this.lstPhotoType.SelectedIndexChanged += new System.EventHandler(this.lstPhotoType_SelectedIndexChanged);
+            // 
+            // lstPhoto
+            // 
+            this.lstPhoto.Location = new System.Drawing.Point(192, 19);
+            this.lstPhoto.Name = "lstPhoto";
+            this.lstPhoto.Size = new System.Drawing.Size(504, 556);
+            this.lstPhoto.TabIndex = 1;
+            this.lstPhoto.UseCompatibleStateImageBehavior = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(988, 618);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 644);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnInsert;
+        private System.Windows.Forms.Button BtnOpenFile;
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.ListBox lstPhotoType;
+        private System.Windows.Forms.ListView lstPhoto;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
--- FrmMyAlum練習17111914/Form1.cs
+++ FrmMyAlum練習17111914/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using FrmMyAlum練習17111914.Properties;
+using System.IO;
 
 namespace FrmMyAlum練習17111914
 {
@@ -16,5 +19,76 @@ namespace FrmMyAlum練習17111914
         {
             InitializeComponent();
         }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            using (var cn = new SqlConnection(Settings.Default.Photo))
+            using (var adp = new SqlDataAdapter(@"
+                  SELECT DISTINCT
+                    phototype
+                  FROM
+                    phototable;", cn))
+            {
+                var dt = new DataTable();
+                adp.Fill(dt);
+                var allRow = dt.NewRow();
+                allRow["PhotoType"] = "全部";
+                dt.Rows.InsertAt(allRow, 0);
+
+                lstPhotoType.DataSource = dt;
+            }
+        }
+
+        private void lstPhotoType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var photoType = (string)lstPhotoType.SelectedValue;
+            var sqlText = string.Empty;
+
+            if (photoType == "全部")
+            {
+                sqlText = @"select 
+                                Id, 
+                                Photo 
+                            from 
+                                PhotoTable;";
+            }
+            else
+            {
+                sqlText = @"select 
+                                Id, 
+                                Photo 
+                            from 
+                                PhotoTable
+                            where
+                                PhotoType =@type;";
+
+            }
+            using (var cn = new SqlConnection(Settings.Default.Photo))
+            using (var adp = new SqlDataAdapter(sqlText, cn))
+            {
+                adp.SelectCommand.Parameters.Add("type", SqlDbType.NVarChar).Value = photoType;
+                var dt = new DataTable();
+                adp.Fill(dt);
+                var imgList = new ImageList();
+                imgList.ImageSize = new Size(100, 100);
+                lstPhoto.LargeImageList = imgList;
+                lstPhoto.Items.Clear();
+
+                var imgs = from row in dt.AsEnumerable()
+                           select new
+                           {
+                               Id = row.Field<int>("Id").ToString(),
+                               Pic = Image.FromStream(new MemoryStream(row.Field<byte[]>("Photo")))
+                           };
+                foreach(var item in imgs)
+                {
+                    imgList.Images.Add(item.Id, item.Pic);
+                    var lisItem = lstPhoto.Items.Add(item.Id);
+                    lisItem.ImageKey = item.Id;
+                }
+
+            }
+        } 
+
     }
-}
+}   
